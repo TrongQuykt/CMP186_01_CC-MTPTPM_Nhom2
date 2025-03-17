0b8c6f85d392beef4c6a5d384801194335559e7d
F-E-A-R <vyquy633@gmail.com>
2025-03-17 08:15:58 +0700

    Ngày 2: Thêm bảng Message trong Database + thêm Message trong Model

---
+++ gearvn/src/main/java/com/example/ecommerce/gearvn/model/Message.java
@@ -0,0 +1,37 @@
+package com.example.ecommerce.gearvn.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long senderId; // Người gửi
+    private Long receiverId; // Người nhận
+    private String message;
+    private String username;
+    private LocalDateTime timestamp;
+    private String status;
+
+    // Getters và Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Long getSenderId() { return senderId; }
+    public void setSenderId(Long senderId) { this.senderId = senderId; }
+    public Long getReceiverId() { return receiverId; }
+    public void setReceiverId(Long receiverId) { this.receiverId = receiverId; }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
